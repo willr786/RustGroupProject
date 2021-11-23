78fc682e916f80f2cc64b3bd8ffc4a00125672bf
Henry Fiantaca <hfiantaca@gmail.com>
2021-11-22 21:26:39 -0500

    Make UI use self.history in self.render()

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::io;
-use tui::Terminal;
-use tui::backend::CrosstermBackend;
 mod ui;
 
 fn main() {
-
     let mut ui = ui::UI::new(io::stdout()).expect("ui could not be inizilized");
-    
-    let mut history = vec!["test1".to_string(), "test2".to_string(), "test3".to_string()];
-    ui.render("test".to_string(),"test".to_string());
+    // TODO: input system
 
+    ui.add_history("test1".to_string());
+    ui.add_history("test2".to_string());
+    ui.add_history("test3".to_string());
+
+    ui.render("test".to_string(), "test".to_string());
 }
