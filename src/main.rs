c24e4db073ae5d424814036c2fb44ae0960ac6fc
Henry Fiantaca <hfiantaca@gmail.com>
2021-11-28 17:48:17 -0500

    Typo

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 mod ui;
 
 fn main() {
-    let mut ui = ui::UI::new(io::stdout()).expect("ui could not be inizilized");
+    let mut ui = ui::UI::new(io::stdout()).expect("ui could not be initialized");
     // TODO: input system
 
     ui.add_history("test1".to_string());
